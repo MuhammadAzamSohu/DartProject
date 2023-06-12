35852f60f9ce3b50952c074c5e62a0ea3877559e
MuhammadAzamSohu <muhammadazamsohu36@gmail.com>
2023-06-12 18:30:55 +0500

    user authentication system

---
+++ test.dart
@@ -0,0 +1,38 @@
+void main()
+{
+
+
+var email="muhammadazamsohu35@gmail.com";
+var password=123;
+
+
+
+if(password!=123)
+{
+print("password incroorect please try again   ");
+
+
+}
+if(email!="muhammadazamsohu36@gmail.com")
+{
+print("email incorrect");
+
+}
+if(email=="muhammadazamsohu36@gmail.com" && password==123)
+{
+  print("login success");
+
+
+
+
+}
+
+
+
+else
+
+{
+  print("failed");
+}
+
+}
